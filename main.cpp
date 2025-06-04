cd584c68f70c4d14adfefa83a34a7e487331d26d
Aibar Yelemessov <aibarelemesov7@gmail.com>
2025-06-04 18:46:04 +0100

    add socket listening port 8080

--- main.cpp
+++ main.cpp
@@ -1,16 +1,51 @@
 #include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <netinet/in.h>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    int server_fd, client_fd;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i == " << i << std::endl;
+    // Create socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0) {
+        perror("socket failed");
+        return 1;
     }
 
+    // Allow port reuse
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    // Bind to port
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        return 1;
+    }
+
+    listen(server_fd, 3);
+    std::cout << "Listening on port 8080..." << std::endl;
+
+    client_fd = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+    if (client_fd < 0) {
+        perror("accept");
+        return 1;
+    }
+
+    read(client_fd, buffer, 1024);
+    std::cout << "Received: " << buffer << std::endl;
+
+    std::string response = "Hello from server\n";
+    send(client_fd, response.c_str(), response.length(), 0);
+
+    close(client_fd);
+    close(server_fd);
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
